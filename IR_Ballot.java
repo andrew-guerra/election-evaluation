7bf742b84e4e25a77d289a122f1199f5972fb423
Tyler Grimm <grimm223@umn.edu>
2023-03-15 10:42:26 -0500

    implemented IR_Ballot

---
+++ IR_Ballot.java
@@ -0,0 +1,47 @@
+public class IR_Ballot { 
+    private int ballotNum; 
+    private int rank; 
+    private char[] form; 
+    private int[] candidateRanking; 
+
+    public IR_Ballot(int ballotNum, int rank, char[] form, int[] candidateRanking){ 
+        this.ballotNum = ballotNum;
+        this.rank = rank;
+        this.form = form; 
+        this.candidateRanking = candidateRanking;
+    }
+
+    public int getBallotNum() {
+        return this.ballotNum;
+    }
+
+    public void setBallotNum(int ballotNum) {
+        this.ballotNum = ballotNum;
+    }
+
+    public int getRank() {
+        return this.rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public char[] getForm() {
+        return this.form;
+    }
+
+    public void setForm(char[] form) {
+        this.form = form;
+    }
+
+    public int[] getCandidateRanking() {
+        return this.candidateRanking;
+    }
+
+    public void setCandidateRanking(int[] candidateRanking) {
+        this.candidateRanking = candidateRanking;
+    }
+
+}
+    
