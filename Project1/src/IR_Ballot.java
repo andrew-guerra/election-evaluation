f4b82d27a6139552692d26fc07fac24cb89031fe
Hady Kotifani <kotif004@umn.edu>
2023-03-21 10:09:48 -0500

    Completed Candidate and IR_Ballot classes

--- Project1/src/IR_Ballot.java
+++ Project1/src/IR_Ballot.java
@@ -1,16 +1,29 @@
+// imports if needed
+// added toString
+// added constructor with more fields
+
 public class IR_Ballot { 
     private int ballotNum; 
     private int rank; 
-    private char[] form; 
+    private String form; 
     private int[] candidateRanking; 
 
-    public IR_Ballot(int ballotNum, int rank, char[] form, int[] candidateRanking){ 
+    // constructors
+    public IR_Ballot() {
+        ballotNum = 0;
+        rank = 0;
+        form = "";
+        candidateRanking = null;
+    }
+
+    public IR_Ballot(int ballotNum, int rank, String form, int[] candidateRanking) {
         this.ballotNum = ballotNum;
         this.rank = rank;
-        this.form = form; 
+        this.form = form;
         this.candidateRanking = candidateRanking;
     }
 
+    // all getters and setters
     public int getBallotNum() {
         return this.ballotNum;
     }
@@ -27,11 +40,11 @@ public class IR_Ballot {
         this.rank = rank;
     }
 
-    public char[] getForm() {
+    public String getForm() {
         return this.form;
     }
 
-    public void setForm(char[] form) {
+    public void setForm(String form) {
         this.form = form;
     }
 
@@ -41,6 +54,17 @@ public class IR_Ballot {
 
     public void setCandidateRanking(int[] candidateRanking) {
         this.candidateRanking = candidateRanking;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "{" +
+            " ballotNum='" + getBallotNum() + "'" +
+            ", rank='" + getRank() + "'" +
+            ", form='" + getForm() + "'" +
+            ", candidateRanking='" + getCandidateRanking() + "'" +
+            "}";
     }
 
 }
