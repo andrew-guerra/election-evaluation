7e095b1df5243d7cb00616d683bc02ea6fdf4fc1
bagwe012 <bagwe012@umn.edu>
2023-03-21 10:08:39 -0500

    CPL_framework

---
+++ Project1/src/CPL_Election.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CPL_Election extends Election {
+
+    private int numParties;
+    private Party[] parties;
+    private int numSeats;
+    private int availableSeats;
+    private CPL_Ballot[] initialBallots;
+    private CPL_Audit audit;
+
+
+    public CPL_Election(Scanner electionFile) {
+        
+    }
+    public void run() {
+
+    }
+    private ArrayList<Party> setFirstSeatsAllocation() {
+        return null;
+    }
+    private void setRemainderSeatsAllocation(ArrayList<Party> partiesAvailable) {
+
+    }
+    public void readCPLHeader() {
+
+    }
+    public void readCPLBallots() {
+
+    }
+    public int getNumParties() {
+
+    }
+    public void setNumParties() {
+
+    }
+    public int getNumSeats() {
+
+    }
+    public int setNumSeats() {
+
+    }
+}
---
+++ Project1/src/Election.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Election {
+
+    protected int numBallots;
+    protected String typeElection;
+    protected String fileName;
+    protected Scanner electionFile;
+
+    public Election(int numBallots, String typeElection, String fileName, Scanner electionFile) {
+        this.numBallots = numBallots;
+        this.typeElection = typeElection;
+        this.fileName = fileName;
+        this.electionFile = electionFile;
+    }
+
+    public int getNumBallots() {
+        return this.numBallots;
+    }
+
+    public void setNumBallots(int numBallots) {
+        this.numBallots = numBallots;
+    }
+
+    public String getTypeElection() {
+        return this.typeElection;
+    }
+
+    public void setTypeElection(String typeElection) {
+        this.typeElection = typeElection;
+    }
+
+    public String getFileName() {
+        return this.fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public Scanner getElectionFile() {
+        return this.electionFile;
+    }
+
+    public void setElectionFile(Scanner electionFile) {
+        this.electionFile = electionFile;
+    }   
+
+}
