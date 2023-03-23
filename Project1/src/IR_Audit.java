b1605c3b9397a0a9ac14541efd2d1b44c2ae451d
Andrew Guerra <guerr242@umn.edu>
2023-03-23 10:40:11 -0500

    Implement base functions of audit classes

---
+++ Project1/src/IR_Audit.java
@@ -0,0 +1,28 @@
+package Project1.src;
+
+/**
+ * IR_Audit is an ElectionAudit for representing an audit for an IR election
+ * 
+ * @author Andrew Guerra
+ */
+public class IR_Audit extends ElectionAudit {
+
+    @Override
+    public String getWinners() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getWinners'");
+    }
+
+    @Override
+    public void writeToAudit() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'writeToAudit'");
+    }
+
+    @Override
+    public void returnAudit() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'returnAudit'");
+    }
+    
+}
