3a5462b2fd886a8bb98d8aec8243ab3eb526f6a2
Andrew Guerra <guerr242@umn.edu>
2023-03-26 13:27:10 -0500

    Fixed commented out unit tests

--- Project1/misc/run-tests.sh
+++ Project1/misc/run-tests.sh
@@ -1,9 +1,9 @@
 javac -d target -cp .:./lib/* ./src/main/*.java ./src/test/unit/*.java ./src/test/system/*.java 
 cd target
 
-#echo "Running Unit Tests"
-#numUnitTest=$(ls -1 test/unit | wc -l)
-#ls test/unit | bash ../misc/read-class-names.sh .class ${numUnitTest} | bash ../misc/append-strings.sh test.unit. ${numUnitTest} | xargs java -cp .:../../lib/*:src/* org.junit.runner.JUnitCore 
+echo "Running Unit Tests"
+numUnitTest=$(ls -1 test/unit | wc -l)
+ls test/unit | bash ../misc/read-class-names.sh .class ${numUnitTest} | bash ../misc/append-strings.sh test.unit. ${numUnitTest} | xargs java -cp .:../../lib/*:src/* org.junit.runner.JUnitCore 
 
 echo "Running System Tests"
 numUnitTest=$(ls -1 test/system | wc -l)
