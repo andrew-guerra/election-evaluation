5f249537e32ef4be54b92783b26514bcf967db3d
Andrew Guerra <guerr242@umn.edu>
2023-03-26 17:00:09 -0500

    Build javadocs

---
+++ Project1/documentation/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"main","l":"Candidate"},{"p":"main","l":"CPL_Audit"},{"p":"main","l":"CPL_Ballot"},{"p":"main","l":"CPL_Election"},{"p":"main","l":"Election"},{"p":"main","l":"IR_Audit"},{"p":"main","l":"IR_Ballot"},{"p":"main","l":"IR_Election"},{"p":"main","l":"Main"},{"p":"main","l":"Party"}]
