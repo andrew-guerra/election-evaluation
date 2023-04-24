4af7b34af29a8c48dd5b09be859eb8ad1bf7b730
Tyler Grimm <grimm223@umn.edu>
2023-04-23 21:30:43 -0500

    IR Test for Mulitple Files

--- Project2/src/test/unit/IR_ElectionTests_MultipleFiles.java
+++ Project2/src/test/unit/IR_ElectionTests_MultipleFiles.java
@@ -20,4 +20,37 @@ import main.IR_Ballot;
 
 public class IR_ElectionTests_MultipleFiles {
     
+    @Test
+    public void testIR_Multiple_Election_headers_and_ballots() throws IOException {
+
+        Scanner electionFile1 = new Scanner(new FileInputStream("../testing/unit/IR_Election/IR_multiFile_header1.csv"));
+        Scanner electionFile2 = new Scanner(new FileInputStream("../testing/unit/IR_Election/IR_multiFile_header2.csv"));
+        Scanner[] electionFiles = {electionFile1, electionFile2};
+
+        electionFiles[0].next();
+        electionFiles[0].nextLine();
+
+        String date = "testing_multiHeader_IR";
+        IR_Election ir = new IR_Election(electionFiles, date);
+        ir.run();
+
+    }
+
+    @Test
+    public void testIR_Multiple_Election_headers_stress_test() throws IOException {
+
+        Scanner electionFile1 = new Scanner(new FileInputStream("../testing/unit/IR_Election/DoubleDigitCandidates.csv"));
+        Scanner electionFile2 = new Scanner(new FileInputStream("../testing/unit/IR_Election/DoubleDigitCandidates.csv"));
+        Scanner[] electionFiles = {electionFile1, electionFile2};
+
+        electionFiles[0].next();
+        electionFiles[0].nextLine();
+
+        String date = "testing_multiIR_stress_header_and_ballot";
+        IR_Election ir = new IR_Election(electionFiles, date);
+        ir.run();
+
+    }
+
+
 }
